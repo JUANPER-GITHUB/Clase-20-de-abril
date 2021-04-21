9fb2fb3d9793af42beafabdc2d4cea73bb2c586d
JUANPER-GITHUB <80792459+JUANPER-GITHUB@users.noreply.github.com>
2021-04-20 19:49:06 -0500

    Add files via upload

---
+++ Funcion_callable_punto4.py
@@ -0,0 +1,4 @@
+print(callable([1, 2, 3]))
+print(callable(callable))
+print(callable(False))
+print(callable(list))
---
+++ Funcion_len_punto4.py
@@ -0,0 +1,6 @@
+# Ejemplo de la funcion len()     -    La funcion len solo lee texto
+print("Ejemplo contando texto: ")
+print(len("Juan Perez"))
+print("Ejemplo contando numeros: ")
+print(len("1234556789"))
+
